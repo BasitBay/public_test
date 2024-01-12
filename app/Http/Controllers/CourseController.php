14ae1ea79903d12059c7444b78ce798865a40e77
My Life`s Work <bruce.lee1940@outlook.com>
2024-01-12 13:16:49 +0100

    create a course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 use App\Models\Course;
 
@@ -12,5 +12,37 @@ class CourseController extends Controller
     {
         $courses = Course::orderBy('created_at', 'desc')->get();
         return view('courses.index', compact('courses'));
+    }
+
+    //Retreive total number of courses to display them later in the navbar
+    public function displayTotalCourses()
+    {
+        $totalCourses = Course::count();
+
+        return view('layouts.course_layout', compact('totalCourses'));
+    }
+
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validation logic here
+        $request->validate([
+            'title' => 'required|min:2|max:256',
+            'participants' => 'required|integer',
+            'price' => 'nullable',
+        ]);
+
+        $course = Course::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'max_participants' => $request->input('participants'),
+        ]);
+
+        return redirect()->route('courses.index')->with('success', 'Course created successfully');
     }
 }
